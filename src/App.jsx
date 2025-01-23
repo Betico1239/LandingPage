5d8ca31dd019081ae479854ff9ffad2a5ebdb9ea
Betico1239 <cristianalberto.bravo@ustabuca.edu.co>
2025-01-23 01:02:05 -0500

    Seccion Downloads y Footer, termina proyecto, sujeto a cambios

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Pricing />
         <Faq />
         <Testimonials />
+        <Download />
+        <Footer />
       </main>
     </div>
   );
