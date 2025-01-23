8c577cd765155be14ecc5c4d5300b1fc345f9da7
Betico1239 <cristianalberto.bravo@ustabuca.edu.co>
2025-01-22 23:07:43 -0500

    Seccion Pricing, con sus funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Hero />
         <Features />
+        <Pricing />
       </main>
     </div>
   );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,9 @@ import Button from "../components/Button.jsx";
 const Hero = () => {
   return (
     <section
-      className="relative mb-10 pt-60 max-lg:pt-52 max-lg:pb-36
-       max-md:pt-36 max-md:pb-32"
+      id="hero"
+      className="relative mb-10 pt-60 max-lg:pt-52 max-lg:pb-36 max-md:pt-36
+      max-md:pb-32"
     >
       <Element name="Hero">
         <div className="container">
